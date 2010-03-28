4c2a3914ad74fb857ac7f10272a68a935851ac14
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-03-28 14:30:01 +0200

    Added two test file templates as a TODO; they will need to be filled with content later on.

---
+++ tests/test_commands.py
@@ -0,0 +1,10 @@
+"""TOOD: We need to test the basic command functionality, ensuring that
+at it's core, import, export and init are operative, create the files they
+should create, skip the files they should skip when they should be skipped,
+etc. In particular, we should test both the case of multiple XML input files
+(strings.xml, arrays.xml), and the case of only single source.
+
+"test_options" tests the commands in combination with specific options and
+will thus ensure that commands run, but does not check that they do the
+right thing.
+"""
